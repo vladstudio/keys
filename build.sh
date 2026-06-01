97204995a9cea6390f87dfe497c74fcdb04768e1
Vlad Gerasimov <vlad@vlad.studio>
2026-06-01 15:44:16 +0400

    Rename scripts/ to app-scripts/

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-source ../scripts/build-kit.sh
+source ../app-scripts/build-kit.sh
 build_app "Keys" \
   --resources "Sources/Keys/Resources/*.png" \
   --entitlements Keys.entitlements
